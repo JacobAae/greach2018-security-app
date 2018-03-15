98383133a60746380567e3d980d81a325c3e80ef
Unknown <jam@cardlay.com>
2018-03-15 11:55:33 +0100

    remove println

--- grails-app/controllers/net/grydeske/security/OtherLoginController.groovy
+++ grails-app/controllers/net/grydeske/security/OtherLoginController.groovy
@@ -5,8 +5,6 @@ class OtherLoginController {
     OtherLoginService otherLoginService
 
     def index(String name, String password) {
-        println "name=$name password=$password"
-
         def isAllowedIn = otherLoginService.isAllowedAccess(name,password)
 
         [name:name, password:password, isAllowedIn: isAllowedIn]
